f91724fe097866c939b05b1d9033c9413490f720
sgerodes <sgerodes@gmail.com>
2022-07-23 16:35:32 +0200

    make setup version variable

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup
+import subprocess
+import os
+
+scrapy_dot_items_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+assert os.path.isfile("cf_remote/version.py")
+with open("scrapy_dot_items/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % scrapy_dot_items_version)
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+
 
 setup(
     name='scrapy-dot-items',
     packages=['scrapy_dot_items',
               ],
-    version='0.1.1',
+    # version='0.1.1',
+    version=scrapy_dot_items_version,
     description='A Scrapy addon that allows to access arguments via the dot',
     url='https://github.com/sgerodes/scrapy-items',
     author='Sergey Gerodes',
@@ -18,10 +33,10 @@ setup(
     package_dir={'': 'src'},
     package_data={"scrapy_dot_items": ["VERSION"]},
     keywords=['python', 'scrapy', 'scrapy-items', 'dot-items', 'dot'],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
